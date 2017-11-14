a1ae5557a84f1f391aec4ba1cf6e27dff1dbfdbd
Jamie Luxford <jl15219@my.bristol.ac.uk>
2017-11-14 14:32:01 +0000

    adjusted main to work with buttons

--- script/src/Main.purs
+++ script/src/Main.purs
@@ -1,15 +1,32 @@
 module Main where
 
 import Prelude
+
+import Data.Tuple
 import Data.List
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
-import Data.DOM.Simple.Unsafe.Element
-import Data.DOM.Simple.Unsafe.Events
-import Data.DOM.Simple.Unsafe.Window
-import Data.DOM.Simple.Types
-import Data.DOM.Simple.Window
-import DOM
+import React as R
+import React.DOM as R
+import React.DOM.Props as RP
+import Thermite hiding (defaultMain) as T
+import Thermite.Try as T
+
+type State = Tuple (List Braille) (Braille)
+
+nullBraille :: Braille
+nullBraille = Letter O O O O O O
+
+initialState :: State
+initialState = Tuple Nil nullBraille
+
+-- Here is the action type associated with our component:
+
+data Bool = True | False
+
+data Action = Increment Int | Decrement Bool
+
+data Dot = O | X
+
+data Braille = Letter Dot Dot Dot Dot Dot Dot
 
 brailleToString :: List Braille -> List Char
 brailleToString Nil = Nil
@@ -64,6 +81,101 @@ brailleLetter2String (Letter X X O X X X) = 'y'
 brailleLetter2String (Letter X O O X X X) = 'z'
 brailleLetter2String otherwise = '\t'
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-    log "Hello Sailor"
+
+-- The first argument to the render function is a callback which
+-- can be used to invoke actions.
+--
+-- Notice how the action gets attached to event handlers such as
+-- onClick.
+
+render :: T.Render State _ _
+render dispatch _ state _ =
+  [ R.h1' [ R.text "Lesson 2 - Actions" ]
+  , R.p' [ R.text "The state is: "
+         , R.text (show ((brailleToString (fst state))))
+         ]
+  , R.p [ RP.className "btn-group" ]
+        [ R.button [ RP.className "btn btn-success"
+                   , RP.onClick \_ -> dispatch (Increment 1)
+                   ]
+                   [ R.text "Increment" ]
+        , R.button [ RP.className "btn btn-danger"
+                   , RP.onClick \_ -> dispatch (Increment 2)
+                   ]
+                   [ R.text "Decrement" ]
+        , R.button [ RP.className "btn btn-danger"
+                   , RP.onClick \_ -> dispatch (Increment 3)
+                   ]
+                   [ R.text "Decrement" ]
+        ]
+  , R.p [ RP.className "btn-group" ]
+        [ R.button [ RP.className "btn btn-success"
+                   , RP.onClick \_ -> dispatch (Increment 4)
+                   ]
+                   [ R.text "Increment" ]
+        , R.button [ RP.className "btn btn-danger"
+                   , RP.onClick \_ -> dispatch (Increment 5)
+                   ]
+                   [ R.text "Decrement" ]
+        , R.button [ RP.className "btn btn-danger"
+                   , RP.onClick \_ -> dispatch (Increment 6)
+                   ]
+                   [ R.text "Decrement" ]
+        ]
+  , R.p [ RP.className "btn-group" ]
+        [ R.button [ RP.className "btn btn-danger"
+                   , RP.onClick \_ -> dispatch (Decrement False)
+                   ]
+                   [ R.text "Decrement" ]
+        , R.button [ RP.className "btn btn-danger"
+                   , RP.onClick \_ -> dispatch (Decrement True)
+                   ]
+                   [ R.text "Decrement" ]
+        ]
+  , R.p'  [ R.text "Go to "
+          , R.a [ RP.href "?gist=0e1b6ed00421ae4ae7b17a919c267199&backend=thermite"
+                , RP.target "_top"
+                ]
+                [ R.text "Lesson 3" ]
+          , R.text "."
+          ]
+  ]
+
+-- The performAction function is responsible for responding to an action
+-- by returning a coroutine which emits state updates.
+--
+-- A simple coroutine emits a single state update using the 'cotransform'
+-- function.
+--
+-- The coroutine type can also be used asynchronously, as we will see in
+-- the next lesson.
+
+flipDot :: Dot -> Dot
+flipDot O = X
+flipDot X = O
+
+adjustBraille :: Int -> Braille -> Braille
+adjustBraille 1 (Letter a b c d e f) = Letter (flipDot a) b c d e f
+adjustBraille 2 (Letter a b c d e f) = Letter a (flipDot b) c d e f
+adjustBraille 3 (Letter a b c d e f) = Letter a b (flipDot c) d e f
+adjustBraille 4 (Letter a b c d e f) = Letter a b c (flipDot d) e f
+adjustBraille 5 (Letter a b c d e f) = Letter a b c d (flipDot e) f
+adjustBraille 6 (Letter a b c d e f) = Letter a b c d e (flipDot f)
+adjustBraille n (Letter a b c d e f) = Letter a b c d e f
+
+removeBraille :: List Braille -> List Braille
+removeBraille Nil = Nil
+removeBraille xs = reverse (f (reverse xs)) where
+    f :: List Braille -> List Braille
+    f Nil = Nil
+    f (x:xs) = xs
+
+performAction :: T.PerformAction _ State _ Action
+performAction (Increment n) _ _ = void $ T.modifyState $ \(Tuple state1 state2) -> Tuple (state1) (adjustBraille n state2)
+performAction (Decrement True) _ _ = void $ T.modifyState $ \(Tuple state1 state2) -> Tuple (state1 <> (state2:Nil)) nullBraille
+performAction (Decrement False) _ _ = void $ T.modifyState $ \(Tuple state1 state2) -> Tuple (removeBraille state1) state2
+
+spec :: T.Spec _ State _ Action
+spec = T.simpleSpec performAction render
+
+main = T.defaultMain spec initialState
